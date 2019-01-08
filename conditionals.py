ca5667894f4de6e2788885e9b9179736b0437139
DennisOnder <webdev.dennisonder@gmail.com>
2019-01-08 21:37:04 +0100

    Conditionals v2

--- conditionals.py
+++ conditionals.py
@@ -16,3 +16,16 @@ if x > 2:
 # Logical operators (and, or, not)
 if x > 2 and x <= 10:
     print(f'{x} is qreater than 10, but less than or equal to 10.')
+
+# Membership operators
+
+numbers = [1,2,3,4,5]
+
+if x in numbers:
+    print(x in numbers)
+elif x not in numbers:
+    print(x not in numbers)
+
+# Identity operators (is, is not)
+if x is y:
+    print(x is y)
