76dd66862402160574d1b76b8b50f5dbcbb92344
DennisOnder <webdev.dennisonder@gmail.com>
2019-01-08 15:42:59 +0100

    Variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+# Variables
+
+x = 1             # Integer
+y = 2.5           # Float
+name = 'John Doe' # String
+is_cool = True    # Boolean
+
+# Multiple Assignment
+
+x2, y2, name2, is_cool2 = (1, 2.5, 'Dan Joe', False)
+
+# Printing
+
+print(x, x2)
+print(y, y2)
+print(name, name2)
+print(is_cool, is_cool2)
+
+# Casting
+
+x3 = int(x)
+y3 = str(y)
+
+print(type(x3), x)
+print(type(y3), y)
