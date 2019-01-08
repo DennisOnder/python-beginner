985537b2298b8ff8daa9413369fd5c320a922e48
DennisOnder <webdev.dennisonder@gmail.com>
2019-01-08 16:20:18 +0100

    Tuples/Sets

---
+++ tuples_sets.py
@@ -0,0 +1,23 @@
+# Unchangeable list
+
+# Create tuple
+fruits = ('Apples', 'Mangos', 'Bananas')
+fruits2 = tuple(('Apples', 'Mangos', 'Bananas'))
+
+# Get value
+print(fruits[1])
+
+# Can't changge values - example: fruits[0] = 'Whatever'
+
+# Sets
+fruits_set = {'Apples', 'Mangos', 'Bananas'}
+
+# Check if in set
+print('Apples' in fruits_set)
+
+# Add to set
+fruits_set.add('Grapes')
+
+# Clear entire set
+fruits_set.clear()
+print(fruits_set)
